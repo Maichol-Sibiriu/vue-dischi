5794bc1d872290ba69ee6c0e57b1f4010aa08a25
Maichol <msibiriu@gmail.com>
2020-11-24 17:27:10 +0100

    ok stampa delle api,ora inizio filtraggio

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@ const js = new Vue({
   el: '#app',
   data:{
 
-
+    listCd: [],
 
   },
   created(){
 
-    axios.get('')
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then( response => {
         // handle success
-        console.log(response);
+        console.log(response.data.response);
+        this.listCd = response.data.response;
       })
       .catch( error => {
         // handle error
